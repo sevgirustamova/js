ea4f6784f33f7a24238f36402d299b98a71c4e56
sevgirustamova <sevgirmva>
2025-08-18 15:32:48 +0400

    code

---
+++ burgermenyu/main.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+
---
+++ task-200/main.js
@@ -0,0 +1,10 @@
+// 2. İstifadəçidən adını soruşub salam verin
+
+// let name = prompt(" Zehmet olmasa,adinizi daxil edin.")
+// console.log("Salam " + name);
+
+// 4. Düzbucaqlının sahəsini hesablayan funksiya
+function sahe(en, uzunlug) {
+    return en * uzunlug
+}
+console.log(sahe(5, 8));
